54a10e2abdcdd57a217021db396c26348ab34388
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-07 17:03:55 +0100

    Update icons
    Remove AMP Code
    
    
    Fix code in the wrong place
    
    
    Always 100

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
 export default function IndexPage() {
-  return (
-    <div>
-      <h1>Hello, World </h1>
-    </div>
-  )
+	return (
+		<>
+			<Head>
+				<title>Easy DMARC</title>
+				<meta
+					name="Description"
+					content="A tool to make adding DMARC to a site easy"
+				/>
+			</Head>
+			<div>
+				<h1>Hello, World </h1>
+			</div>
+		</>
+	);
 }
