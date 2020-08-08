13f62bc17f73b811c795ccfde8283f9869108711
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-08 15:19:05 +0100

    Generate SPF record

--- pages/form.js
+++ pages/form.js
@@ -5,6 +5,10 @@ export default function Form() {
 	const [subdomainPolicy, setSubdomainPolicy] = useState("none");
 	const [email, setEmail] = useState("");
 	const [dmarc, setDmarc] = useState("");
+	const [spf, setSpf] = useState("");
+	const [inDomain, setInDomain] = useState("");
+	const [outDomain, setOutDomain] = useState("");
+	const [strict, setStrict] = useState("~all");
 	function changeEmail(event) {
 		setEmail(event.target.value);
 	}
@@ -14,6 +18,15 @@ export default function Form() {
 
 	function subPolicy(event) {
 		setSubdomainPolicy(event.target.value);
+	}
+	function indomain(event) {
+		setInDomain(event.target.value);
+	}
+	function outdomain(event) {
+		setOutDomain(event.target.value);
+	}
+	function strictVal(event) {
+		setStrict(event.target.value);
 	}
 	useEffect(() => {
 		setDmarc(
@@ -26,6 +39,22 @@ export default function Form() {
 				"; sp=" +
 				subdomainPolicy +
 				"; ri=86400"
+		);
+		console.log(inDomain.split(/\r?\n/));
+		setSpf(
+			"v=spf1 mx " +
+				inDomain
+					.split(/\r?\n/)
+					.filter((item) => item)
+					.map((elem) => " a:" + elem)
+					.join("") +
+				outDomain
+					.split(/\r?\n/)
+					.filter((item) => item)
+					.map((elem) => " include:" + elem)
+					.join("") +
+				" " +
+				strict
 		);
 	});
 	return (
@@ -66,9 +95,42 @@ export default function Form() {
 							onChange={changeEmail}
 						/>
 					</label>
+					<label className="block">
+						Servers in the domain that can send email:
+						<textarea
+							type="text"
+							className="form-textarea"
+							value={inDomain}
+							onChange={indomain}
+						/>
+					</label>
+					<label className="block">
+						External domains that may deliver or relay mail:
+						<textarea
+							type="text"
+							className="form-textarea"
+							value={outDomain}
+							onChange={outdomain}
+						/>
+					</label>
+					<div className="pb-4">
+						How strict should the servers be treating the emails:
+						<select
+							className="form-select"
+							value={strict}
+							onChange={strictVal}
+						>
+							<option value="-all">Fail</option>
+							<option value="~all">Soft Fail</option>
+							<option value="?all">Neutral</option>
+							<option value="+all">Allow all</option>
+						</select>
+					</div>
 				</form>
 			</div>
-			{dmarc}
+			<p>DMARC: {dmarc}</p>
+			<br />
+			<p>SPF: {spf}</p>
 		</>
 	);
 }
