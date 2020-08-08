c9adcaa172c59310868393b0eaae1ab11b069ec4
Tom Nudd <thomasgnudd@gmail.com>
2020-08-08 15:10:24 +0100

    grab access tokens
    need "ID" in .env.local

--- pages/login.js
+++ pages/login.js
@@ -5,6 +5,20 @@ export default function Login({ data }) {
 
 export async function getServerSideProps(context) {
 	const data = context.query.code;
+
+	fetch("https://api.vercel.com/v2/oauth/access_token", {
+		method: "POST",
+		body: "client_id=" + encodeURIComponent(process.env.ID) +
+			  "&client_secret=" + encodeURIComponent(process.env.SECRET) +
+			  "&code=" + encodeURIComponent(data) +
+			  "&redirect_uri=" + encodeURIComponent("http://localhost:3000/login"),
+		headers: {
+				"Content-Type": "application/x-www-form-urlencoded"
+			}
+		}
+	).then(rsp => rsp.json())
+	.then(rsp => console.log(rsp));
+
 	console.log(process.env.SECRET);
 	return { props: { data } };
 }
