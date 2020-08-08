3569277bb8055939e09a3e0796f3e0d488dd1c51
Tom Nudd <thomasgnudd@gmail.com>
2020-08-08 16:10:07 +0100

    set cookie "token" containing access token

--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,43 @@
 import { useRouter } from "next/router";
-export default function Login({ data }) {
-	return <h1>Hello</h1>;
+import { parseCookies, setCookie, destroyCookie } from "nookies";
+export default function Login({ cookies }) {
+	return (
+		<div>
+			<h1>Hello</h1>
+			<ul>
+				{ cookies && Object.entries(cookies).map(([name, val]) => (
+					<li key={name}>
+						{name}: {val}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export async function getServerSideProps(context) {
 	const data = context.query.code;
+	const cookies = parseCookies(context);
 
-	fetch("https://api.vercel.com/v2/oauth/access_token", {
+	const rsp = await fetch("https://api.vercel.com/v2/oauth/access_token", {
 		method: "POST",
 		body: "client_id=" + encodeURIComponent(process.env.ID) +
 			  "&client_secret=" + encodeURIComponent(process.env.SECRET) +
 			  "&code=" + encodeURIComponent(data) +
 			  "&redirect_uri=" + encodeURIComponent("http://localhost:3000/login"),
 		headers: {
-				"Content-Type": "application/x-www-form-urlencoded"
+				"Content-Type": "application/x-www-form-urlencoded",
 			}
 		}
-	).then(rsp => rsp.json())
-	.then(rsp => console.log(rsp));
+	);
+	
+	const body = await rsp.json();
+	const token = body["access_token"];
+	
+	setCookie(context, "token", token, {
+		maxAge: 60 * 60 * 24,
+		path: "/",
+	});
 
-	console.log(process.env.SECRET);
-	return { props: { data } };
+	return { props: { cookies } }
 }
