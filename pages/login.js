5535cbe897899695ff7ad28a20c472be6f7ce49f
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-08 13:27:08 +0100

    More work on login

--- pages/login.js
+++ pages/login.js
@@ -1,1 +1,10 @@
-export default function Login() {}
+import { useRouter } from "next/router";
+export default function Login({ data }) {
+	return <h1>Hello</h1>;
+}
+
+export async function getServerSideProps(context) {
+	const data = context.query.code;
+	console.log(process.env.SECRET);
+	return { props: { data } };
+}
