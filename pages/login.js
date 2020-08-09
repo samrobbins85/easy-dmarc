7b01d2480e99618e2f302ad07a47c188ef39efe2
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-09 09:54:06 +0100

    Fetch all domains

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,40 @@
 import { useRouter } from "next/router";
 import { parseCookies, setCookie, destroyCookie } from "nookies";
 import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
 
 export default function Login({ cookies }) {
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [items, setItems] = useState([]);
+	useEffect(() => {
+		fetch("https://api.vercel.com/v5/domains", {
+			headers: {
+				Authorization: "Bearer " + Cookies.get("token"),
+			},
+		})
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					setIsLoaded(true);
+					console.log(result.domains.map((item) => item.name));
+					setItems(result.domains.map((item) => item.name));
+				},
+
+				(error) => {
+					setIsLoaded(true);
+					setError(error);
+				}
+			);
+	}, []);
 	return (
 		<div>
 			<h1>Hello</h1>
-
+			<ul>
+				{!error &&
+					isLoaded &&
+					items.map((item) => <li key={item}>{item}</li>)}
+			</ul>
 			{Cookies.get("token")}
 		</div>
 	);
