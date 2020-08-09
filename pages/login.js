a1712161bbdfc5b4b62db0f1e50378e611a443c4
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-09 10:41:34 +0100

    Add form to login page

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import { parseCookies, setCookie, destroyCookie } from "nookies";
 import Cookies from "js-cookie";
 import { useState, useEffect } from "react";
@@ -7,6 +6,43 @@ export default function Login({ cookies }) {
 	const [error, setError] = useState(null);
 	const [isLoaded, setIsLoaded] = useState(false);
 	const [items, setItems] = useState([]);
+	const [domain, setDomain] = useState("");
+	const [policy, setPolicy] = useState("none");
+	const [subdomainPolicy, setSubdomainPolicy] = useState("none");
+	const [email, setEmail] = useState("");
+	const [dmarc, setDmarc] = useState("");
+	const [spf, setSpf] = useState("");
+	const [inDomain, setInDomain] = useState("");
+	const [outDomain, setOutDomain] = useState("");
+	const [strict, setStrict] = useState("~all");
+
+	useEffect(() => {
+		setDmarc(
+			"v=DMARC1; p=" +
+				policy +
+				`${email && "; rua=mailto:" + email}` +
+				`${email && "; ruf=mailto:" + email}` +
+				"; sp=" +
+				subdomainPolicy +
+				"; ri=86400"
+		);
+		setSpf(
+			"v=spf1 mx " +
+				inDomain
+					.split(/\r?\n/)
+					.filter((item) => item)
+					.map((elem) => " a:" + elem)
+					.join("") +
+				outDomain
+					.split(/\r?\n/)
+					.filter((item) => item)
+					.map((elem) => " include:" + elem)
+					.join("") +
+				" " +
+				strict
+		);
+	});
+
 	useEffect(() => {
 		fetch("https://api.vercel.com/v5/domains", {
 			headers: {
@@ -17,7 +53,6 @@ export default function Login({ cookies }) {
 			.then(
 				(result) => {
 					setIsLoaded(true);
-					console.log(result.domains.map((item) => item.name));
 					setItems(result.domains.map((item) => item.name));
 				},
 
@@ -28,15 +63,85 @@ export default function Login({ cookies }) {
 			);
 	}, []);
 	return (
-		<div>
-			<h1>Hello</h1>
-			<ul>
-				{!error &&
-					isLoaded &&
-					items.map((item) => <li key={item}>{item}</li>)}
-			</ul>
-			{Cookies.get("token")}
-		</div>
+		<>
+			<div className="flex justify-center pt-6">
+				<form>
+					<div class="grid grid-cols-2 gap-3">
+						<p>Domain:</p>
+						<select
+							className="form-select"
+							value={domain}
+							onChange={() => setDomain(event.target.value)}
+						>
+							{!error &&
+								isLoaded &&
+								items.map((item) => (
+									<option value={item}>{item}</option>
+								))}
+						</select>
+						<p>Policy:</p>
+						<select
+							className="form-select"
+							value={policy}
+							onChange={() => setPolicy(event.target.value)}
+						>
+							<option value="none">None</option>
+							<option value="quarantine">Quarantine</option>
+							<option value="reject">Reject</option>
+						</select>
+						<p>Policy for Subdomains:</p>
+						<select
+							className="form-select"
+							value={subdomainPolicy}
+							onChange={() =>
+								setSubdomainPolicy(event.target.value)
+							}
+						>
+							<option value="none">None</option>
+							<option value="quarantine">Quarantine</option>
+							<option value="reject">Reject</option>
+						</select>
+						<p>Email:</p>
+						<input
+							type="text"
+							className="form-input"
+							value={email}
+							onChange={() => setEmail(event.target.value)}
+						/>
+						<p>Servers in the domain that can send email:</p>
+						<textarea
+							type="text"
+							className="form-textarea"
+							value={inDomain}
+							onChange={() => setInDomain(event.target.value)}
+						/>
+						<p>External domains that may deliver or relay mail:</p>
+						<textarea
+							type="text"
+							className="form-textarea"
+							value={outDomain}
+							onChange={() => setOutDomain(event.target.value)}
+						/>
+						<p>
+							How strict should the servers be treating the
+							emails:
+						</p>
+						<select
+							className="form-select"
+							value={strict}
+							onChange={() => setStrict(event.target.value)}
+						>
+							<option value="-all">Fail</option>
+							<option value="~all">Soft Fail</option>
+							<option value="?all">Neutral</option>
+							<option value="+all">Allow all</option>
+						</select>
+					</div>
+				</form>
+			</div>
+			<p>Dmarc{dmarc}</p>
+			<p>SPF:{spf}</p>
+		</>
 	);
 }
 
