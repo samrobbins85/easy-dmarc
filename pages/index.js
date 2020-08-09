5839055029ac5f2f3ac25646407b745b553b7f04
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-09 11:52:36 +0100

    Start work on designing homepage

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,37 @@ export default function IndexPage() {
 					content="A tool to make adding DMARC to a site easy"
 				/>
 			</Head>
-			<div className="text-2xl text-center text-blue-600">
-				<a href="https://vercel.com/oauth/authorize?client_id=oac_rWwV143gK1BrtELznk58Ylio">
-					Sign In
-				</a>
+			<div className="flex justify-center pt-6">
+				<div className="grid">
+					<h1 className="text-5xl font-semibold pb-12 text-center">
+						Easy DMARC
+					</h1>
+					<p className="text-lg pb-12  px-6 text-center">
+						Easily add DMARC to any of your domains hosted on Vercel
+					</p>
+					<div className="flex justify-center">
+						<a
+							className="bg-black p-4 text-white font-semibold rounded flex"
+							href="https://vercel.com/oauth/authorize?client_id=oac_rWwV143gK1BrtELznk58Ylio"
+						>
+							<svg
+								className="h-6"
+								viewBox="0 0 116 100"
+								fill="#fff"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fillRule="evenodd"
+									clipRule="evenodd"
+									d="M57.5 0L115 100H0L57.5 0z"
+								/>
+							</svg>
+							<span className="inline-block px-8">
+								Login with Vercel
+							</span>
+						</a>
+					</div>
+				</div>
 			</div>
 		</>
 	);
